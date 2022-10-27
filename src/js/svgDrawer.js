a0e7ecc9735843e8defe2198eaa157b43388cf6c
Carlos Zamora <czamora5@ucol.mx>
2022-10-26 22:34:24 -0500

    refactor: remake hero animation using css instead of javascript

--- src/js/svgDrawer.js
+++ src/js/svgDrawer.js
@@ -1,36 +1,5 @@
-// export const observer = new IntersectionObserver(entries => {
-//   entries.forEach(entry => {
-//     console.log(entry);
-//   });
-// });
-
-const section = document.querySelector('.hero');
-const path = document.querySelector('.hero__figure__left path');
-const path2 = document.querySelector('.hero__figure__right path');
-const length = path.getTotalLength();
-const length2 = path2.getTotalLength();
-path.style.strokeDasharray = length;
-path.style.strokeDashoffset = length;
-path2.style.strokeDasharray = length2;
-path2.style.strokeDashoffset = length2;
-
-const endAt = 0.6;
-const endAt2 = 0.8;
-
-document.addEventListener('scroll', () => {
-  const domRect = section.getBoundingClientRect();
-  const percentageScrolled = Math.abs(domRect.top / (domRect.height - window.innerHeight));
-
-  if (percentageScrolled <= endAt) {
-    path.style.strokeDashoffset = length + length * percentageScrolled / endAt;
-    path2.style.strokeDashoffset = length2 + length2 * percentageScrolled / endAt;
-  } else if (percentageScrolled <= endAt2) {
-    const rate = 100;
-    path.style.transform = `translateY(${-rate * (percentageScrolled - endAt) / (endAt2 - endAt)}px)`;
-    path2.style.transform = `translateY(${rate * (percentageScrolled - endAt) / (endAt2 - endAt)}px)`;
-  } else {
-    const rate = 100;
-    path.style.transform = `translate(${-rate * (percentageScrolled - endAt2) / (1.0 - endAt2)}px, -100px)`;
-    path2.style.transform = `translate(${rate * (percentageScrolled - endAt2) / (1.0 - endAt)}px, 100px)`;
-  }
-});
+const leftPath = document.querySelector('.hero__figure__left path');
+const rightPath = document.querySelector('.hero__figure__right path');
+leftPath.style.setProperty('--path-length', leftPath.getTotalLength() + 'px');
+leftPath.style.setProperty('--factor', -1);
+rightPath.style.setProperty('--path-length', rightPath.getTotalLength() + 'px');
