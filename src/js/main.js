5f714bbf8cfd3f329fee1e47b5a8a8383aa9c061
Carlos Zamora <czamora5@ucol.mx>
2022-10-28 21:58:38 -0500

    refactor: set display none on the page content during initial animation instead of just hidding overflow

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
 import './svgDrawer.js';
 import { imgs } from './data.js';
 
-window.addEventListener('beforeunload', () => {
-  window.scrollTo(0, 0);
-});
-
 function appendImages(containers) {
   imgs.forEach((url, index) => {
     const img = document.createElement('img');
