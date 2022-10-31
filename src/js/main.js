2863b6a7b709a913dcad404a14f03cd7207f78d2
Carlos Zamora <czamora5@ucol.mx>
2022-10-31 12:33:31 -0600

    fix: remove hover interaction in devices with no hover support and fix favicon path

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,16 @@ import { imgs } from './data.js';
 
 function appendImages(containers) {
   imgs.forEach((url, index) => {
+    const figure = document.createElement('figure');
+    figure.classList.add('grid__figure');
+
     const img = document.createElement('img');
     img.classList.add('grid__img');
     img.src = url;
-    containers[index % containers.length].appendChild(img);
+    img.alt = 'AI generated highly photorealistic face';
+
+    figure.appendChild(img);
+    containers[index % containers.length].appendChild(figure);
   });
 }
 
